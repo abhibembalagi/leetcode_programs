883c503623032924cae6427046707394cb6a7943
abhibembalagi <51333444+abhibembalagi@users.noreply.github.com>
2021-02-05 18:10:39 +0530

    Update two_sum_sorted_array.py

--- two_sum_sorted_array.py
+++ two_sum_sorted_array.py
@@ -1,1 +1,10 @@
 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        first_number={}
+        for n,m in enumerate(numbers):
+            sec_number = target -m
+            if sec_number in first_number:
+                return (int(first_number[sec_number])+1,(n+1))
+            first_number[m]=n
+        return  []
